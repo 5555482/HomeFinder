a4395476551f872436f4deacfb429b3020106c8f
Natalie Antonova <5555482@gmail.com>
2016-12-05 14:12:14 +0000

    search page

---
+++ SearchPage.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableHighlight,
+  ActivityIndicator,
+  Image
+} from 'react-native';
+
+class SearchPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>
+          Search for houses to buy!
+        </Text>
+        <Text style={styles.description}>
+          Search by place-name, postcode or search near your location.
+        </Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+	description: {
+		marginBottom: 20,
+		fontSize: 18,
+		textAlign: 'center',
+		color: '#656565'
+	},
+	container: {
+		marginTop: 65,
+		alignItems: 'center'
+	}
+});
+
+module.exports = SearchPage;
---
+++ index.ios.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+var ReactNative = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} = require('react-native');
+var SearchPage = require('./SearchPage');
+
+class HomeFinder extends React.Component {
+  render() {
+    return (
+      <ReactNative.NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Home Finder',
+          component: SearchPage
+        }}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  text: {
+    backgroundColor: '#F5FCFF',
+    margin: 80,
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('HomeFinder', () => HomeFinder);
